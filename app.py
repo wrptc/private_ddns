99dd8339e4321fd34203d8b3d6c2897e9db85253
wangruipeng <wrptc.wang@gmail.com>
2020-04-30 17:47:36 +0800

    改用format-string兼容python3.5

--- app.py
+++ app.py
@@ -5,21 +5,25 @@ from flask import request, Flask
 config = json.load(open('config.json'))
 PASSWD = config['sudo_passwd']
 HOSTS = config['hosts']
+app = Flask(__name__)
+
 
 def sh(command):
-    os.system(f'echo {PASSWD} | sudo -S {command}')
+    os.system('echo {} | sudo -S {}'.format(PASSWD, command))
+
 
 def update_hosts(host, dest):
-    sh(f'sed -i \'/{host}/d\' {HOSTS}')
-    sh(f'sed -i \'$a\\{dest}  {host}\' {HOSTS}')
-    
-app = Flask(__name__)
+    sh('sed -i \'/{}/d\' {}'.format(host, HOSTS))
+    sh('sed -i \'$a\\{}  {}\' {}'.format(dest, host, HOSTS))
+
+
 @app.route('/hosts', methods=['POST'])
 def hosts():
     host = request.form.get('host')
     dest = request.form.get('dest')
     update_hosts(host, dest)
     return 'OK'
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
